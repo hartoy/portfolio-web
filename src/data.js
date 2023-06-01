9803fd31c73730017568691cfafc495ff7b3378e
Ignacio Hardoy <hartoyy@hotmail.com>
2023-06-01 18:40:25 -0300

    proyect creation

---
+++ src/data.js
@@ -0,0 +1,180 @@
+export const dataSkills = [
+  { id: '1', name: 'HTML' },
+  { id: '2', name: 'CSS' },
+  { id: '3', name: 'JSX' },
+  { id: '4', name: 'REACT' },
+  { id: '5', name: 'LARAVEL' },
+  { id: '6', name: 'PHP' },
+  { id: '7', name: 'GIT' },
+  { id: '8', name: 'JAVASCRIPT' },
+  { id: '9', name: 'SCRUM' },
+  { id: '10', name: 'MYSQL' },
+]
+
+export const dataExp = [
+  {
+    id: '1',
+    name: 'Genosha - Desarrollador Web',
+    puesto: '',
+    dateIn: 'Marzo 2022',
+    dateEnd: 'Abril 2023',
+    description:
+      'Maquetado web desde Figma, Photoshop o Illustrator.Markup bugfixing de web apps.Colaboración con equipos de desarrollo backend en la integración de API’s.Exploración tecnologías frontend como ReactJs.',
+    link: 'https://www.linkedin.com/company/genosha/',
+  },
+  {
+    id: '2',
+    name: 'ONG Somos Mas',
+    puesto: 'Desarrollador de front-end',
+    dateIn: 'Septiembre 2021',
+    dateEnd: 'Enero 2022',
+    description: 'Desarrollo en equipo de sitio para ONG Somos Mas utilizando React JS, Jira, y Github',
+    link: 'https://github.com/alkemyTech/OT104-Client',
+  },
+  {
+    id: '3',
+    name: 'Digital House - Cuerdas',
+    puesto: 'Desarrollador Full Stack',
+    dateIn: 'Julio 2019',
+    dateEnd: 'Diciembre 2019',
+    description:
+      'Desarrollo de una ecommerce utilizando HTML para la estructura, CSS para el diseño y JS para interacciones y/o validaciones del lado del cliente. - El diseño y desarrollo del FrontEnd toma las buenas prácticas de hacer sitios responsive. Se uso MySQL para la base de datos. ',
+    link: 'https://github.com/simonyagas/proyectoDH-Sprint05',
+  },
+  {
+    id: '4',
+    name: 'Arquitecto Leon Nilsson',
+    puesto: 'Desarrollador Full Stack',
+    dateIn: 'Octubre 2020',
+    dateEnd: 'Diciembre 2020',
+    description: 'Desarrollo de sitio web responsive utilizando HTML, CSS y JS.',
+    link: 'https://github.com/hartoy/Arq-Leon-Nilsson',
+  },
+  {
+    id: '5',
+    name: '',
+    puesto: 'Desarrollador Full Stack',
+    dateIn: 'Agosto 2020',
+    dateEnd: 'Noviembre 2020',
+    description: 'Desarrollo de sitio web responsive utilizando HTML ,CSS ,JS y Laravel.',
+    link: 'https://github.com/hartoy/sysmic',
+  },
+]
+
+export const outExp = [
+  {
+    id: '1',
+    name: 'Administracion Cristina Gonzalez',
+    puesto: 'Empleado administrativo',
+    dateIn: 'Enero 2012',
+    dateEnd: 'Marzo 2023',
+  },
+  {
+    id: '2',
+    name: 'Somos Deporte',
+    puesto: 'Moviles para television con cobertura de futbol,hockey y rugby',
+    dateIn: 'Enero 2013',
+    dateEnd: 'Diciembre 2013',
+  },
+  {
+    id: '3',
+    name: 'LC Control de plagas',
+    puesto: 'Empleado administrativo',
+    dateIn: 'Enero 2011',
+    dateEnd: 'Diciembre 2012',
+  },
+]
+
+export const proyects = [
+  {
+    id: '1',
+    name: 'Fan Page Tampa Bay Buccaneers',
+    tech: 'HTML - CSS - JS - PHP',
+    linkHub: 'https://github.com/hartoy/Bucs',
+    link: '',
+  },
+  {
+    id: '2',
+    name: 'MovieFlix',
+    tech: 'REACT',
+    linkHub: 'https://github.com/hartoy/react-movieflix',
+    link: 'https://hartoy-movieflix-llprznlf0-hartoy.vercel.app/',
+  },
+  {
+    id: '3',
+    name: 'SPACE',
+    tech: 'HTML - CSS - JS',
+    linkHub: 'https://github.com/hartoy/space-test',
+    link: 'https://cetia.com.ar/',
+  },
+  {
+    id: '4',
+    name: 'CountDown',
+    tech: 'HTML - CSS - JS',
+    linkHub: 'https://github.com/hartoy/countdown',
+    link: 'https://cetia.com.ar/',
+  },
+]
+
+export const formation = [
+  {
+    id: '1',
+    name: 'Digital House Coding School',
+    puesto: 'Programacion Web Full Stack',
+    dateIn: 'Junio 2019',
+    dateEnd: 'Diciembre 2019',
+    link: 'https://www.digitalhouse.com/ar',
+  },
+  {
+    id: '2',
+    name: 'The Full Stack Dev',
+    puesto: 'Manejo del DOM y React',
+    dateIn: 'Junio 2020',
+    dateEnd: 'Diciembre 2020',
+    link: 'https://www.instagram.com/thefullstackdevs/',
+  },
+  {
+    id: '3',
+    name: 'ISEC',
+    puesto: 'Periodismo Deportivo',
+    dateIn: 'Enero 2010',
+    dateEnd: 'Diciembre 2013',
+    link: 'https://www.isec.edu.ar/periodismodeportivo/',
+  },
+  {
+    id: '5',
+    name: 'CETIA',
+    puesto: 'Tecnico armado y reparacion de PC',
+    dateIn: 'Junio 2019',
+    dateEnd: 'Diciembre 2019',
+    link: 'https://cetia.com.ar/',
+  },
+]
+
+export const personalData = [
+  {
+    birthday: '19/12/91',
+    nacionalidad: 'ARG - ESP',
+    localidad: 'ARG - Buenos Aires - Capital Federal',
+    email: 'hartoyy@hotmail.com',
+    num: '1556926173',
+    idioma: 'Esp nativo - Ingles B2',
+  },
+]
+
+export const socialsLinks = [
+  {
+    linkedin: 'https://www.linkedin.com/in/ignacio-hardoy-14b575a0/',
+    github: 'https://github.com/hartoy',
+    ig: 'https://www.instagram.com/nachohardoy/',
+  },
+]
+
+export const personalText = [
+  {
+    obj: 'Mi objetivo es seguir creciendo profesionalmente, contribuyendo con mi experiencia y conocimientos en proyectos desafiantes y colaborando con equipos apasionados por la creación de soluciones innovadoras.',
+    textLong:
+      'Como programador frontend con más de un año de experiencia en proyectos reales, he desarrollado habilidades sólidas en el uso de React para la creación de interfaces de usuario eficientes y atractivas. Además, tengo experiencia en el manejo de Laravel y PHP en el backend.He utilizado herramientas de control de versiones como Git, así como metodologías ágiles y herramientas de gestión de proyectos como Trello o Jira para colaborar de manera efectiva en equipos de desarrollo. Estoy comprometido con la mejora continua y en mantenerme actualizado con las últimas tendencias y tecnologías en el campo del desarrollo web.',
+    textShort: '',
+  },
+]
